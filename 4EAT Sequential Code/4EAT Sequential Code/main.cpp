9fcff44f9996593f73f57deeff0f683dede3952f
Nicholas Layton <nick_layton@msn.com>
2018-07-24 15:46:24 -0500

    code folder
    added xcode project folder

---
+++ 4EAT Sequential Code/4EAT Sequential Code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  4EAT Sequential Code
+//
+//  Created by Nicholas Layton on 7/24/18.
+//  Copyright © 2018 HSC Performance. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
